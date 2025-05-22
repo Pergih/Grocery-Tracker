9cfd341df1780dce783b779067ac6672babc44d1
Pergih <pedromo5555@gmail.com>
2025-05-23 00:39:17 +0100

    better bd schema

--- scripts/crud.py
+++ scripts/crud.py
@@ -1,22 +1,39 @@
 import sqlite3
 
-conn = sqlite3.connect("../data/grocery.db")
+conn = sqlite3.connect("data/grocery.db")
 cursor = conn.cursor()
 
 
 def create_tables():
     cursor.execute('''
-        CREATE TABLE IF NOT EXIST products (
+        CREATE TABLE IF NOT EXISTS canonical_products (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
+            name TEXT NOT NULL
         )
     ''')
     
     cursor.execute('''
-        CREATE TABLE IF NOT EXIST prices (
+        CREATE TABLE IF NOT EXISTS brands (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL
+        )
+    ''')
+    
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS products (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            scraped_name TEXT NOT NULL,
+            canonical_id INTEGER,
+            brand_id INTEGER,
+            FOREIGN KEY (canonical_id) REFERENCES canonical_product(id),
+            FOREIGN KEY (brand_id) REFERENCES brand(id)
+        )
+    ''')
+    
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS prices (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             product_id INTEGER,
-            store TEXT,
             price REAL,
             date TEXT,
             FOREIGN KEY (product_id) REFERENCES product(id)  
@@ -24,14 +41,29 @@ def create_tables():
     ''')
     conn.commit()
 
-def add_product(name):
-    cursor.execute("INSERT INTO products (name) VALUES (?,?)", (name))
+def add_canonical_product(name):
+    cursor.execute("INSERT INTO canonical_products (name) VALUES (?)", (name,))
     conn.commit()
     
-def add_price(product_id, store, price, date):
-    cursor.execute("INSERT INTO prices (product_id, store, price, date) VALUES (?, ?, ?, ?)",
-                   (product_id, store, price, date))
+def add_brand(name):
+    cursor.execute("INSERT OR IGNORE INTO brands (name) VALUES (?)", (name,))
     conn.commit()
+    
+    cursor.execute("SELECT id FROM brands WHERE name = ?", (name,))
+    return cursor.fetchone()[0]
+    
+def add_product(scraped_name, canonical_id, brand_id):
+    cursor.execute("INSERT INTO products (scraped_name, canonical_id, brand_id) VALUES (?,?,?)", (scraped_name, canonical_id, brand_id))
+    conn.commit()
+    
+def add_price(product_id, price, date):
+    cursor.execute("INSERT INTO prices (product_id, store, date) VALUES (?, ?, ?)",
+                   (product_id, price, date))
+    conn.commit()
+    
+def get_all_brands():
+    cursor.execute("SELECT * FROM brands")
+    return cursor.fetchall()
     
 def get_all_products():
     cursor.execute("SELECT * FROM products")
@@ -58,7 +90,4 @@ def delete_product(product_id):
 def delete_old_prices(before_date):
     cursor.execute("DELETE FROM prices WHERE date < ?", (before_date,))
     conn.commit()
-
-
-
 
--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,6 @@
-# main.py
 import crud
 
 crud.create_tables()
-crud.add_product("Eggs", "Dairy")
-print(crud.get_all_products())
+crud.add_brands("Continente")
+crud.add_canonical_product("Arroz")
+print(crud.get_all_brands())
